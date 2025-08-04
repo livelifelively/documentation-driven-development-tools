e9e035ed535b5ce21f8afd4df5f4c3631e27aa30
ashish rana <dev.livelifelively@gmail.com>
2025-08-04 07:41:00 +0530

    script fix to align with ESM port

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,8 @@
 import * as path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export const TODO_PLACEHOLDER_TEXT = '_ADD_CONTENT_HERE_';
 
